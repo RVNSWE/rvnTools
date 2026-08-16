9b6270ba733980b0a127ffa43a6a39a8affbba0e
RVNSWE <monkey_lady@hotmail.com>
2026-08-16 14:44:47 +0100

    Added tube feeding component, updated readme.

---
+++ rvnTools/src/components/TubeFeeding.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <title>Tube Feeding</title>
+  
+    <div>
+        <h1>Generate Feeding Plan</h1>
+        <p>This is the Tube Feeding component.</p>
+    </div>
+</template>
