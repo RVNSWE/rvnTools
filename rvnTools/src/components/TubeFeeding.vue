9bf2fc33ec58e4f3fa4280af32602e178e9c32c4
RVNSWE <monkey_lady@hotmail.com>
2026-08-16 22:47:45 +0100

    Converted tube feeding input form to TS.

--- rvnTools/src/components/TubeFeeding.vue
+++ rvnTools/src/components/TubeFeeding.vue
@@ -3,7 +3,12 @@ import { ref } from 'vue'
 
 defineProps(['title'])
 
-let volumes = ref({
+let data = ref({
+    bodyWeight: 0,
+    dietName: '',
+    kcalPerGram: 0,
+    dietNetWeight: 0,
+    dietWaterPercentage: 0,
     day: 0,
     flushVolume: 0,
     feedVolume: 0,
@@ -19,7 +24,7 @@ let diluted = ref(false);
 function processData() {
     calculating.value = true;
     
-    // create data object here
+    // create data object here?
 
         //data.FlushVolume = data.BodyWeight switch
         //{
@@ -38,7 +43,7 @@ function processData() {
     for (let day = 1; day < days.value; day++) {
         // create volumes for today
         // calculate volumes
-        calculatedVolumes.value.set(day, volumes.value);
+        calculatedVolumes.value.set(day, data.value);
     }
 
     calculating.value = false;
@@ -54,10 +59,10 @@ function reset() {
 function foodContainerText() {
     let text = '';
     
-    if (volumes.value.containersPerDay > 1) {
+    if (data.value.containersPerDay > 1) {
         text = 'containers';
     }
-    else if (volumes.value.containersPerDay === 1) {
+    else if (data.value.containersPerDay === 1) {
         text = 'container';
     }
     else {
@@ -74,7 +79,50 @@ function foodContainerText() {
     <div v-if="!calculated">
         <h2>Generate Tube Feeding Plan</h2>
 
-        <p>This is the Tube Feeding component.</p>
+        <h3>Patient Information</h3>
+
+        <label for="species">Select species:</label>
+        <select id="species" v-model="speciesSelection">
+            <option value="dog">Dog</option>
+            <option value="cat">Cat</option>
+        </select>
+
+        <label for="bodyWeight">Enter body weight (kg):</label>
+        <input type="number" id="bodyWeight" v-model="data.bodyWeight" />
+
+        <h3>Diet Information</h3>
+
+        <p>
+            If using a liquid diet, enter the kcal/ml and the volume in ml per container instead.
+        </p>
+
+        <label for="dietName">Enter diet name:</label>
+        <input type="text" id="dietName" v-model="data.dietName" />
+
+        <label for="kcalPerGram">Enter kcal per gram:</label>
+        <input type="number" id="kcalPerGram" v-model="data.kcalPerGram" />
+
+        <label for="dietNetWeight">Enter diet net weight (g):</label>
+        <input type="number" id="dietNetWeight" v-model="data.dietNetWeight" />
+
+        <label for="dietWaterPercentage">Enter diet water percentage:</label>
+        <input type="number" id="dietWaterPercentage" v-model="data.dietWaterPercentage" />
+
+        <h3>Feeding information</h3>
+
+        <p>
+            Choose "diluted" if you want to dilute the food with water before drawing it up, or "separate" if you want to
+            administer food and water separately.
+        </p>
+
+        <label for="days">Number of days to re-feed over:</label>
+        <input type="number" id="days" v-model="data.day" />
+
+        <label for="diluted">Dilute food with water or administer separately?:</label>
+        <select id="diluted" v-model="diluted">
+            <option value="diluted">Diluted</option>
+            <option value="separate">Separate</option>
+        </select>
     </div>
 
     <div v-else>
